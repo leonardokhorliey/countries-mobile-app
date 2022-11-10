d21230976eadf24d035450d2ca7c1e895d1accef
leonardokhorliey <ebubeokoli@rocketmail.com>
2022-11-10 09:27:10 +0100

    App bar set up

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,20 +13,27 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Scaffold(
+        backgroundColor: Color(0xFF000f24),
+        body: SafeArea(
+          child: Column(
+
+            children: [
+              Container(
+                width: 400,
+                child: Row(
+                  children: [
+                    Text('Explore.',
+                    style: TextStyle(fontFamily: 'Elsie Swash Caps', color: Colors.white,fontSize: 50)),
+
+                  ]
+                )
+              )
+            ]
+          )
+        )
+      )
+      ,
     );
   }
 }
